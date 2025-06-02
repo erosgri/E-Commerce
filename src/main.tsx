093b60c2ba6931f3d10af2902438fdbef1756e34
erosgri <junior-gri@live.com>
2025-06-02 09:31:23 -0300

    configurando meu app

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.scss';
+import MainRoutes from './routes';
+import { BrowserRouter } from 'react-router-dom';
+
+
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <StrictMode>
+      <MainRoutes />
+    </StrictMode>
+  </BrowserRouter>
+)
